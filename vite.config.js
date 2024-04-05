42af5f6673c1b1d2ccadb5a4b8ff2623183fecf3
Ismam Khan <ismamkhan@Ismams-MacBook-Pro.local>
2024-04-06 01:56:43 +0600

    i

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port:3000,
     proxy:{
         "/api": {
-          target:"http://localhost:8000/api",
+          target: "http://IsmamK.pythonanywhere.com/api",
           changeOrigin:true,
           rewrite: (path)=>path.replace(/^\/api/,"")
         }
